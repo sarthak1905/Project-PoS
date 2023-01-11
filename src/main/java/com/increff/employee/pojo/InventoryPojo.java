c2e528ca854ced13babac844e258e8af94708b7e
Sarthak Saxena <sarthak.saxena1905@gmail.com>
2023-01-11 17:48:21 +0530

    Completed Pojo definitions

--- src/main/java/com/increff/employee/pojo/InventoryPojo.java
+++ src/main/java/com/increff/employee/pojo/InventoryPojo.java
@@ -1,4 +1,33 @@
 package com.increff.employee.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class InventoryPojo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    int quantity;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }
--- src/main/java/com/increff/employee/pojo/OrderPojo.java
+++ src/main/java/com/increff/employee/pojo/OrderPojo.java
@@ -1,4 +1,32 @@
 package com.increff.employee.pojo;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
 public class OrderPojo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private LocalDateTime dateTime;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
 }
