be064b7f9703e0999a3cffa09e1e4a3652c825b3
Sarthak Saxena <sarthak.legendary@gmail.com>
2023-01-24 17:31:46 +0530

    Minor fixes

--- src/main/java/com/increff/employee/pojo/BrandPojo.java
+++ src/main/java/com/increff/employee/pojo/BrandPojo.java
@@ -9,21 +9,17 @@ import javax.persistence.*;
 @Entity
 @Table(indexes ={@Index(columnList = "brand,category")},
         uniqueConstraints = {@UniqueConstraint(columnNames = {"brand", "category"})})
+@Getter
+@Setter
 public class BrandPojo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Integer id;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String brand;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String category;
 
